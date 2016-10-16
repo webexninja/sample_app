9862197788e81aeef6df432d2d9731566a7bccea
webexninja <shivanktiware@gmail.com>
2016-10-16 20:33:47 +0530

    Add users controller, modify route to signup_path, connect it to homepage sihnup link, write integration test for title of signup page, FINISH Filling-In-Layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
 
   get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
